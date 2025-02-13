b190f244cc617a31f1e857fa1c8e7e30309153ab
Anoop Sharma <anoopksharma22@gmail.com>
2025-02-13 16:02:37 +0530

    CrossOrigin added

--- backend/src/main/java/in/goducky/controllers/ExecutionController.java
+++ backend/src/main/java/in/goducky/controllers/ExecutionController.java
@@ -4,18 +4,20 @@ import in.goducky.services.CodeExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import in.goducky.dtos.ExecutionResponse;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/execution")
 public class ExecutionController {
-    @Autowired
+
     CodeExecutor codeExecutor;
+
+    public ExecutionController(CodeExecutor codeExecutor) {
+        this.codeExecutor = codeExecutor;
+    }
 
     @PostMapping("/execute")
     public ResponseEntity<ExecutionResponse> execute(@RequestBody String javaCode) {
