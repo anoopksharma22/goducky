9516759f887657865acab3d36b789179c8f95a22
Anoop Sharma <anoopksharma22@gmail.com>
2025-02-13 19:56:13 +0530

    article ms added

---
+++ article/src/test/java/in/goducky/article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.goducky.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
